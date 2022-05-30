4dc84c8cc489b0904bc548c4e9aee187687f2a71
Toth Tamas <tot.tamas04@gmail.com>
2022-05-30 11:01:36 +0200

    Vegleges

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function load() {
             "<li> " + 
             items[i].price + 
             "</li>"+
-            
+            '<button id="btn_menu">Buy</button>' +
             "</ul>" +
             "</div>";
         }
